a03a0ac5a05ed6185e912d445c6b7fc05c9c05c4
Braa Knaane <braa97@windowslive.com>
2023-03-22 22:30:12 +0200

    changes

--- server/routes/movieApi.js
+++ server/routes/movieApi.js
@@ -8,64 +8,8 @@ router.use("/", function (req, res) {
   res.sendFile(path.join(__dirname, '../../dist/movie'))
 });
 
-<<<<<<< HEAD
 router.get("/", function (req, res) {
   let movieId = req.query.id
-=======
-router.get("/movies", function (req, res) {
-  const querys = {};
-  const rating = req.query.rating;
-  const year = req.query.year;
-  const genre = req.query.genre;
- 
-  let  page = 1 
-  const limit =  10
-  const startIndex = (page -1 ) * limit
-  const endIndex = page * limit
-  
-  if (rating) {
-    querys.rating = {$gt: rating}
-  }
-  if (year) {
-    querys.year = year;
-  }
-  if (genre) {
-    querys.genre = genre;
-  }
-
-  movieQuery.getMovies(querys)
-    .then((data) => {
-      const paginatedData = data.slice(startIndex, endIndex)
-      console.log(paginatedData);
-      res.status(200).send({
-        total: data.length,
-        page,
-        limit,
-        data: paginatedData
-      });
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-});
-
-
-router.get("/movie/:movieName", function (req, res) {
-  const movie = req.params.movieName;
-  movieQuery.getMovieByName(movie)
-    .then((data) => {
-      res.status(200).send(data);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-});
-
-
-
-router.post("/movie" , function (req, res) {
-  let movieId = req.body.id;
->>>>>>> b7c2946e8c8bf9228432e32ae852f09c3ada2d81
   movieQuery.getMovieById(movieId)
   .then((data) => {
     res.status(200).send(data);
